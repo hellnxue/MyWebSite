e087e66d13fde0edd7e3d8577f9870d1692025c6
hellnxue <1032755304@qq.com>
2016-12-20 20:23:39 +0800

    底部弹出层总结

---
+++ 我的新站点.site/CSS课程/js/myJs.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function(){
+	
+    $("#layerTest").on("click",function(){
+
+		layerShow();
+
+	});
+
+ 	$("div[data-outer]").on("click",function(){
+
+		layerHide();							 
+	});
+
+});
+
+
+
+
+function layerShow(){
+
+	$("div[data-outer]").removeClass("hidden");
+	$("div[data-layer]").removeClass("hidden")//.css("bottom","-200px");
+
+}
+
+
+function layerHide(){
+	
+	$("div[data-outer]").addClass("hidden");
+	$("div[data-layer]").addClass("hidden")//.css("bottom","0");
+}
