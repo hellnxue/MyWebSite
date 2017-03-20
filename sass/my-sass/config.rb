b79984b7d153c81bce740f2e6124ce2ffb8a27a2
hellnxue <1032755304@qq.com>
2017-03-20 17:55:15 +0800

    sass学习

--- sass/my-sass/config.rb
+++ sass/my-sass/config.rb
@@ -8,14 +8,33 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
+
+#缓存文件生成设置
+#cache=false设置sass编译成css文件时根目录会出现.sass-cache文件夹，要重启监听compass watch才奏效奥
+cache=false
+
+#在浏览器调试
+sourcemap = true
+
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+#输出样式output_style :expanded、:nested 、:compact、:compressed 
+#默认的输出风格:expanded表示编译后保持原格式
+#output_style = :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+#取消注释
+line_comments = false
+
+
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# 去掉relative_assets=true 让compass的辅助函数取相对路径
+relative_assets = true
+
+
 
 
 # If you prefer the indented syntax, you might want to regenerate this
@@ -23,3 +42,6 @@ javascripts_dir = "javascripts"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+#config.rb配置文件详解
+#http://blog.csdn.net/qishuixian/article/details/54606436
